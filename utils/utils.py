b0e07081c89ac54dcff2d754ebd2b1007e19808d
minsing-jin <ironman0722@naver.com>
2025-07-11 13:52:57 +0900

    Implement sample upload function

---
+++ utils/utils.py
@@ -0,0 +1,15 @@
+import io
+from PIL import Image
+from meme_convention.db.user import User
+
+def sample_image_upload(context_category, picture_name, path_to_image):
+    user = User()
+    # # Example usage
+    user.upload_meme(context_category, picture_name, path_to_image)
+
+    meme = user.get_random_meme("pr")
+    if meme:
+        img = Image.open(io.BytesIO(bytes(meme[-1])))
+        img.show()
+    else:
+        print("No memes found for the specified category.")
