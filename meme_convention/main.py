58c9afa60870a71dd0d5669d846f1374d950d741
minsing-jin <ironman0722@naver.com>
2025-07-17 16:16:49 +0900

    modify main.py

--- meme_convention/main.py
+++ meme_convention/main.py
@@ -82,4 +82,4 @@ if __name__ == "__main__":
     # app.run()
 
     autocomplete = AutoComplete(analysis_model=None, text=None, page_image=None)
-    autocomplete.gui_display_meme("pr")
+    autocomplete.autocomplete(["pr", "issue", "bug", "feature", "task"])
