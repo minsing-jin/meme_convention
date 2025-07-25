bac035324dc49962a7cbdff40540e22dd9137c8a
minsing-jin <ironman0722@naver.com>
2025-07-25 16:56:01 +0900

    test file not yet implemented

---
+++ test/db/test_local.py
@@ -0,0 +1,20 @@
+from meme_convention.db.local.local import *
+import io
+from PIL import Image
+
+# TODO: this test is not working yet but main function is working. I think it is pytest issue.
+# def test_get_random_meme():
+#     # Example usage
+#     local_db = LocalDB()
+#     context_folder_path = "/Users/jinminseong/Desktop/meme_convention/resources/db_test"  # Replace with your actual folder path
+#
+#     try:
+#
+#         meme_image = local_db.get_random_meme(context_folder_path)
+#         meme_img = Image.open(meme_image)
+#         # meme_io = io.BytesIO(bytes(meme_image))
+#         meme_img.show()  # Display the image
+#     except ValueError as e:
+#         print(f"Error: {e}")
+
+
