5be87d567af909643de64e6803474658637bcc97
minsing-jin <ironman0722@naver.com>
2025-08-01 18:16:18 +0900

    Modify recommender.py to reflect user setting_configuration.

--- meme_convention/recommendar/recommender.py
+++ meme_convention/recommendar/recommender.py
@@ -1,25 +1,29 @@
 from meme_convention.recommendar.text_recorder import TypingRecorder
-from meme_convention.recommendar.image_analyzer import analyze_context_with_gpt
-from meme_convention.recommendar.context_injection_prompt import context_injection_prompt
+from meme_convention.recommendar.multimodal_analyzer import analyze_context_with_gpt
+from meme_convention.recommendar.context_prompt import create_prompt_based_on_text
 from meme_convention.recommendar.screenshot import take_screenshot
+from meme_convention.frontend.context_dialog import ContextCategoryDialog
 
 
-def classify_context_category(context_category_lst: list[str], typing_recorder: TypingRecorder, model: str="gpt-4o-mini",) -> str:
+def classify_context_category(context_category_lst: list[str],
+                              allow_screenshot: bool,
+                              typing_recorder: TypingRecorder = None,
+                              model: str = "gpt-4o-mini") -> str:
     """
     Classify the context category based on the provided context category list.
-
-    Args:
-        context_category_lst (list[str]): List of context categories to choose from.
-
-    Returns:
-        str: The selected context category.
     """
     if not context_category_lst:
         raise ValueError("Context category list is empty.")
 
-    screenshot = take_screenshot()
-    typing_record = typing_recorder.get_last()
-    prompt = context_injection_prompt(context_category_lst, typing_record)
-    selected_context = analyze_context_with_gpt(screenshot, prompt, model)
+    # Collect data
+    typing_record = typing_recorder.get_last() if typing_recorder else ""
+    screenshot = take_screenshot() if allow_screenshot else None
 
-    return selected_context
+    # Decide classification method
+    has_data_for_ai = typing_record or screenshot
+
+    if has_data_for_ai:
+        prompt = create_prompt_based_on_text(context_category_lst, typing_record)
+        return analyze_context_with_gpt(screenshot, prompt, model)
+    else:
+        return ContextCategoryDialog.ask(context_category_lst)
