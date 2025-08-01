1de10549c75c684f0985dc0765258edc3cc3ba03
minsing-jin <ironman0722@naver.com>
2025-08-01 18:20:47 +0900

    Add text recording enable and privacy description

--- meme_convention/recommendar/text_recorder.py
+++ meme_convention/recommendar/text_recorder.py
@@ -1,4 +1,3 @@
-# Check is this safe privacy-wise
 from datetime import datetime, timedelta
 import threading
 from collections import deque
@@ -7,6 +6,14 @@ from pynput import keyboard
 
 # TODO: 영어인지 한국어인지 구별, When typing recoder start to record?
 class TypingRecorder:
+    """
+    Enables local typing recording when using the meme convention.
+
+    If enabled, the model can refer to the user's recent typing (up to 15 seconds before the meme shortcut is triggered)
+    to personalize meme recommendations.
+    Typing data may be sent to the selected model (local or cloud).
+    For sensitive input, it is recommended to keep this option off.
+    """
     def __init__(self, maxlen=500):
         self.typed = deque(maxlen=maxlen)  # automatically discards oldest
         self.lock = threading.Lock()
